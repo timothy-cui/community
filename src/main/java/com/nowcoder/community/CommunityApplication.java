893aed596d121edcb2c2e0771198009fae45b882
timothy-cui <ctiangui@shu.edu.cn>
2022-09-10 13:36:26 +0800

    feat: add contant

--- src/main/java/com/nowcoder/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
 
-@SpringBootApplication
 public class CommunityApplication {
 
     @PostConstruct
